806a6edd4c96544823c61a576c8975fa7536e7f6
Chenu2235 <138844969+Chenu2235@users.noreply.github.com>
2025-01-01 15:06:26 +0530

    Update App.js

--- Chenuthi/src/App.js
+++ Chenuthi/src/App.js
@@ -1,21 +1,31 @@
-// src/App.js
 import React from "react";
-//import LandingPage from "./LandingPage"; // Adjust the path if `LandingPage.js` is in a subdirectory
-//import ServicePage from "./ServicePage";
-//import UploadCVPage from "./UploadCVPage";
-//import MakeYourChoicePage from "./MakeYourChoicePage";
-//import ServiceChoicePage from "./ServiceChoicePage";
-//import VideoPage from "./VideoPage";
-//import EditProfilePage from "./EditProfilePage";
-//import ProfilePage from "./ProfilePage";
-import ServiceChoicePage from "./ServiceChoicePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ServicePage from "./ServicePage"; // Import your ServicePage component
+import TrainerSelection from "./TrainerSelection"; // Import your TrainerSelection component
+import UploadCVPage from "./UploadCVPage"; // Import your UploadCVPage component
+import MakeYourChoicePage from "./MakeYourChoicePage"; // Import your MakeYourChoicePage component
+import VideoPage from "./VideoPage"; // Import your VideoPage component
+import ServiceChoicePage from "./ServiceChoicePage"; // Import your ServiceChoicePage component
 
 function App() {
-  return (
-    <div className="App">
-      <ServiceChoicePage/>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* Route for the ServicePage */}
+                <Route path="/" element={<ServicePage />} />
+                {/* Route for the TrainerSelection */}
+                <Route path="/trainer-selection" element={<TrainerSelection />} />
+                {/* Route for the UploadCVPage */}
+                <Route path="/upload-cv" element={<UploadCVPage />} />
+                {/* Route for the MakeYourChoicePage */}
+                <Route path="/make-your-choice" element={<MakeYourChoicePage />} />
+                {/* Route for the VideoPage */}
+                <Route path="/video" element={<VideoPage />} />
+                {/* Route for the ServiceChoicePage */}
+                <Route path="/service-choice" element={<ServiceChoicePage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
